03ba975dcef5a2980c9601f21fccb7db168faa77
Lucas1269 <lucasabrahao69@gmail.com>
2025-11-14 10:43:42 -0300

    adicionado executavel finalizado menu

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class ClassePrincipal {
 
@@ -16,7 +17,32 @@ public class ClassePrincipal {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
+		Scanner sc = new Scanner(System.in);
+		int opcao;
+		do{
+			limparTela();
+			System.out.println("MENU DE OPÇÃO");
+			System.out.println("[1] Adicionar um aluno");
+			System.out.println("[2] Excluir um aluno");
+			System.out.println("[3] Ver um aluno");
+			System.out.println("[4] Sair");
+			
+			opcao = sc.nextInt();
+			sc.nextLine();
+			limparTela();
+			
+			switch(opcao){
+			case 1:
+				System.out.println("Selecionando opção 1");
+			case 2:
+				System.out.println("Selecionando opção 2");
+			case 3:
+				System.out.println("Selecionando opção 3");
+			default:
+				System.out.println("Opção Padrão");
+			}
+
+		}while(opcao != 4);
 	}
 
 }
